bb7496f2fb41355dc0772da92b01161c23197dcd
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2024-08-24 21:38:26 +0000

    modified:   src/components/Payment.js 	modified:   src/index.js

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import Web3 from 'web3';
 import TelegramGroupManagerABI from '../TelegramGroupManagerABI.json';
 
