1a8bd2b43a1fdf14938a162f41059ee906f90715
emesonmikael <114108397+emesonmikael@users.noreply.github.com>
2024-08-25 23:17:44 +0000

    modified:   src/contractConfig.js

--- src/contractConfig.js
+++ src/contractConfig.js
@@ -2,6 +2,12 @@ import { ethers } from 'ethers';
 
 const CONTRACT_ADDRESS = 'ENDEREÇO_DO_CONTRATO';
 const ABI = [
+  "function editGroup(uint256 groupId, string memory _name, string memory _telegramId, string memory _value, address _wallet) public onlyOwner ",
+  "function pay(uint256 groupId, uint256 amount) public",
+  "function registerGroup(string memory _name, string memory _telegramId, string memory _value, address _wallet) public onlyOwner ",
+  "function setUsdtToken(address _usdtToken) public onlyOwner",
+  "function getGroupCount() public view returns (uint256)",
+  "function getGroup(uint256 groupId) public view returns (string memory, string memory, string memory, address)"
   // Coloque aqui a ABI do seu contrato
 ];
 
